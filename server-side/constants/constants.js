c3361ec6a9d462c82f0945743081f195b7c7451c
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-02-15 21:44:33 +0200

    Added constants.js file in server-side

---
+++ server-side/constants/constants.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const DEFAULT_PORT = 3000;
+
+const SCHEMA_TYPES = {
+    STRING: mongoose.Schema.Types.String,
+    OBJECT_ID: mongoose.Schema.Types.ObjectId,
+};
+
+const ENCRYPTION = {
+    RANDOM_BYTES: 128,
+    BASE_64: 'base64',
+    SHA_256: 'sha256',
+    HEX: 'hex',
+};
+
+const MODELS = {
+    USER: 'User',
+    METRICS: 'Metrics',
+    DAILY_WORKOUTS: 'DailyWorkouts',
+    DAILY_MEALS: 'DailyMeals',
+    ACHIEVEMENTS: 'Achievements',
+}
+
+module.exports = {
+    DEFAULT_PORT,
+    SCHEMA_TYPES,
+    ENCRYPTION,
+    MODELS,
+};
