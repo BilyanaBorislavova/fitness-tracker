d02a00a19e96ed698de060019d9a471373403bf2
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-02-15 21:59:15 +0200

    Updated User model

--- server-side/models/User.js
+++ server-side/models/User.js
@@ -7,24 +7,24 @@ const { USER, METRICS, DAILY_WORKOUTS, DAILY_MEALS, ACHIEVEMENTS } = MODELS;
 
 const userSchema = new mongoose.Schema({
     id: { type: STRING, required: true, unique: true },
+    salt: { type: STRING, required: true },
     username: { type: STRING, required: true, unique: true },
     password: { type: STRING, required: true },
     email: { type: STRING, required: true, unique: true },
     firstName: { type: STRING, required: true },
     lastName: { type: STRING, required: true },
     avatar: { type: STRING, default: '' },
-    roles: [{ type: STRING }],
     metrics: [{ type: OBJECT_ID, ref: METRICS }],
     dailyWorkouts: [{ type: OBJECT_ID, ref: DAILY_WORKOUTS }],
     dailyMeals: [{ type: OBJECT_ID, ref: DAILY_MEALS }],
     achievements: [{ type: OBJECT_ID, ref: ACHIEVEMENTS }],
 });
 
-// userSchema.method({
-//     authenticate: (pass) => (
-//         generateHashedPassword(this.salt, pass) === this.password
-//     )
-// });
+userSchema.method({
+    authenticate: (pass) => (
+        generateHashedPassword(this.salt, pass) === this.password
+    )
+});
 
 const User = mongoose.model(USER, userSchema);
 
