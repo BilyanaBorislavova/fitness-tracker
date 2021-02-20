a7964f6c0bd4f050c5e4083e4bdb8b6f5872c5c8
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-02-20 11:41:38 +0200

    Removed unnecesary id and method from User model

--- server-side/models/User.js
+++ server-side/models/User.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 const { SCHEMA_TYPES, MODELS } = require('../constants/constants');
-const { generateHashedPassword } = require('../utils/encryption');
 
 const { STRING, OBJECT_ID } = SCHEMA_TYPES;
 const { USER, METRICS, DAILY_WORKOUTS, DAILY_MEALS, ACHIEVEMENTS } = MODELS;
 
 const userSchema = new mongoose.Schema({
-    id: { type: STRING, required: true, unique: true },
     salt: { type: STRING, required: true },
     username: { type: STRING, required: true, unique: true },
     password: { type: STRING, required: true },
@@ -14,16 +12,10 @@ const userSchema = new mongoose.Schema({
     firstName: { type: STRING, required: true },
     lastName: { type: STRING, required: true },
     avatar: { type: STRING, default: '' },
-    metrics: [{ type: OBJECT_ID, ref: METRICS }],
-    dailyWorkouts: [{ type: OBJECT_ID, ref: DAILY_WORKOUTS }],
-    dailyMeals: [{ type: OBJECT_ID, ref: DAILY_MEALS }],
-    achievements: [{ type: OBJECT_ID, ref: ACHIEVEMENTS }],
-});
-
-userSchema.method({
-    authenticate: (pass) => (
-        generateHashedPassword(this.salt, pass) === this.password
-    )
+    // metrics: [{ type: OBJECT_ID, ref: METRICS }],
+    // dailyWorkouts: [{ type: OBJECT_ID, ref: DAILY_WORKOUTS }],
+    // dailyMeals: [{ type: OBJECT_ID, ref: DAILY_MEALS }],
+    // achievements: [{ type: OBJECT_ID, ref: ACHIEVEMENTS }],
 });
 
 const User = mongoose.model(USER, userSchema);
