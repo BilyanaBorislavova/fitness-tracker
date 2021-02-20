257a86eba1f05e9f83d315974516480b77e90b0f
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-02-20 11:40:44 +0200

    Added bodyParser

--- server-side/index.js
+++ server-side/index.js
@@ -1,8 +1,19 @@
 const express = require('express')();
+const bodyParser = require('body-parser');
 const database = require('./database/database')();
+const registerRoute = require('./routes/register-route');
 const { DEFAULT_PORT } = require('./constants/constants');
 
 const PORT = process.env.PORT || DEFAULT_PORT;
+
+express.use(bodyParser.json());
+express.use(bodyParser.urlencoded({extended: true}));
+
+express.get('/', (req, res) => {
+    res.send('Working')
+})
+
+express.use('/auth', registerRoute);
 
 express.listen(PORT, () => {
     console.log('App is running successfully!');
