7107c5474f9f004f84c709845aec69aab6de228c
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-03-14 12:30:17 +0200

    Create constants.js

---
+++ client-side/src/constants/constants.js
@@ -0,0 +1,19 @@
+const WHITE_SPACE = ' ';
+
+const INPUT_TYPES = {
+    TEXT: 'text',
+    EMAIL: 'email',
+}
+
+const HTTP_METHODS = {
+    GET: 'GET',
+    POST: 'POST',
+    PUT: 'PUT',
+    DELETE: 'DELETE',
+};
+
+export {
+    WHITE_SPACE,
+    INPUT_TYPES,
+    HTTP_METHODS,
+};
